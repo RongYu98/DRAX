854e1f454518ad12077509d4bcb2bdff232643ea
xiangsgao <xiangsgao@cs.stonybrook.edu>
2020-03-04 02:34:12 -0500

    fixed the background image alignment, sign up link worked now

--- front_end/react_app/src/components/signup.js
+++ front_end/react_app/src/components/signup.js
@@ -11,32 +11,34 @@ class Signup extends React.Component{
 
     render() {
         return (
-            <div id="bg-outer">
-                <div id="bg-inner">
-                </div>
-                <div className="wrap-form">
-                    <form className="form">
-                        <span className="form-title">Create Account for</span>
-                        <div className="wrap-logo btm-space">
-                            <img className="logo" src={Logo} alt="logo" />
-                        </div>
-                        <div className="btm-space">
-                            <div className="wrap-form-input">
-                                <input type="text" name="username" placeholder="Username" minLength={4} maxLength={32} required />
+            <div className='signup_login_container'>
+                <div id="bg-outer">
+                    <div id="bg-inner">
+                    </div>
+                    <div className="wrap-form">
+                        <form className="form">
+                            <span className="form-title">Create Account for</span>
+                            <div className="wrap-logo btm-space">
+                                <img className="logo" src={Logo} alt="logo" />
                             </div>
-                            <p className="helper">* 4 - 32 characters</p>
-                        </div>
-                        <div className="btm-space">
-                            <div className="wrap-form-input">
-                                <input type="password" name="password" placeholder="Password" minLength={8} maxLength={128} required />
+                            <div className="btm-space">
+                                <div className="wrap-form-input">
+                                    <input type="text" name="username" placeholder="Username" minLength={4} maxLength={32} required />
+                                </div>
+                                <p className="helper">* 4 - 32 characters</p>
                             </div>
-                            <p className="helper">* 8 - 128 characters</p>
-                            <p className="helper">* At least 1 number, 1 special character, and both uppercase and lowercase letters</p>
-                        </div>
-                        <div>
-                            <button className="btn btn-primary" type="submit">Sign up</button>
-                        </div>
-                    </form>
+                            <div className="btm-space">
+                                <div className="wrap-form-input">
+                                    <input type="password" name="password" placeholder="Password" minLength={8} maxLength={128} required />
+                                </div>
+                                <p className="helper">* 8 - 128 characters</p>
+                                <p className="helper">* At least 1 number, 1 special character, and both uppercase and lowercase letters</p>
+                            </div>
+                            <div>
+                                <button className="btn btn-primary" type="submit">Sign up</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
