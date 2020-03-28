d2b37356e2ff4eee578fb498c635b508d0acc33f
Amy Yu <amyyu@cs.stonybrook.edu>
2020-03-28 05:51:18 -0400

    Fixed Admission Rate Field

--- backend/file_parser.py
+++ backend/file_parser.py
@@ -170,7 +170,7 @@ def import_college_scorecard(scorecard):
                 institution = institution_type(line[header.index("CONTROL")])
                 adm_rate = line[header.index("ADM_RATE")]
                 if adm_rate != "NULL":
-                    admission_rate = float()*100
+                    admission_rate = float(adm_rate)*100
                 else:
                     admission_rate = adm_rate
                 size = int(line[header.index("UGDS")])
